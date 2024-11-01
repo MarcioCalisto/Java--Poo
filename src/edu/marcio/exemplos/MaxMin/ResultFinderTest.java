16353e85a9cea2ad1dfca5df9f9cb06b8d14e777
Márcio Calisto <calistomarcio6@gmail.com>
2024-10-31 22:03:20 -0300

    Commit

--- src/edu/marcio/exemplos/MaxMin/ResultFinderTest.java
+++ src/edu/marcio/exemplos/MaxMin/ResultFinderTest.java
@@ -1,5 +1,11 @@
 package edu.marcio.exemplos.MaxMin;
 
 public class ResultFinderTest {
-    
+    public static void main(String[] args) {
+        MaxFinder maxfinder = new MaxFinder();
+        maxfinder.determineMax();
+
+        MinFinder minFinder = new MinFinder();
+        minFinder.determineMin();
+    }
 }
