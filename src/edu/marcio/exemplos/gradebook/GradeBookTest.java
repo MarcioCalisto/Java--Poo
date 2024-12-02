807e722906cb647a3e20c1c91863fff3c2d9a540
CalixtoTech <103325008+ClxtMf@users.noreply.github.com>
2024-12-02 18:28:31 -0300

    Update GradeBookTest.java

--- src/edu/marcio/exemplos/gradebook/GradeBookTest.java
+++ src/edu/marcio/exemplos/gradebook/GradeBookTest.java
@@ -3,7 +3,16 @@ package edu.marcio.exemplos.gradebook;
 public class GradeBookTest {
     public static void main(String[] args) {
 
-        GradeBook myGradeBook = new GradeBook(null);     
-        myGradeBook.displayMessage();
-    }
-}
+        // criar objeto GradeBook
+        GradeBook myGradeBook1 = new GradeBook(
+                "CS101 Introduction to Java Programming");
+        GradeBook myGradebook2 = new GradeBook(
+                "CS102 Data Structures in Java");
+
+        // exibe valor inicial de courseName para cada GradeBook
+        System.out.printf("gradeBook1 course name is: %s\n",
+                myGradeBook1.getCourseName());
+        System.out.printf("gradeBook2 course name is: %s\n",
+                myGradebook2.getCourseName());
+    } // fim do método
+} // fim da classe
