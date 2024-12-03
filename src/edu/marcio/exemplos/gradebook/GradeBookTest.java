e7fa9c64eb154d4a5531942f7511a08b2c24e071
CalixtoTech <103325008+ClxtMf@users.noreply.github.com>
2024-12-03 08:53:58 -0300

    Update GradeBookTest.java

--- src/edu/marcio/exemplos/gradebook/GradeBookTest.java
+++ src/edu/marcio/exemplos/gradebook/GradeBookTest.java
@@ -1,18 +1,18 @@
 package edu.marcio.exemplos.gradebook;
 
 public class GradeBookTest {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        // criar objeto GradeBook
-        GradeBook myGradeBook1 = new GradeBook(
-                "CS101 Introduction to Java Programming");
-        GradeBook myGradebook2 = new GradeBook(
-                "CS102 Data Structures in Java");
+                // criar objeto GradeBook
+                GradeBook myGradeBook1 = new GradeBook(
+                                "CS101 Introduction to Java Programming");
+                GradeBook myGradebook2 = new GradeBook(
+                                "CS102 Data Structures in Java");
 
-        // exibe valor inicial de courseName para cada GradeBook
-        System.out.printf("gradeBook1 course name is: %s\n",
-                myGradeBook1.getCourseName());
-        System.out.printf("gradeBook2 course name is: %s\n",
-                myGradebook2.getCourseName());
-    } // fim do método
+                // exibe valor inicial de courseName para cada GradeBook
+                System.out.printf("gradeBook1 course name is: %s\n",
+                                myGradeBook1.getCourseName());
+                System.out.printf("gradeBook2 course name is: %s\n",
+                                myGradebook2.getCourseName());
+        } // fim do método
 } // fim da classe
