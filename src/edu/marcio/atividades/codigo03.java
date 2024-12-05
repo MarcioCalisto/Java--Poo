9dae252409bbc24e7093e4b390f273dea42a2f34
CalixtoTech <103325008+ClxtMf@users.noreply.github.com>
2024-12-05 18:26:57 -0300

    Create codigo03.java

---
+++ src/edu/marcio/atividades/codigo03.java
@@ -0,0 +1,10 @@
+package edu.marcio.atividades;
+
+public class codigo03 {
+    public static void main(String[] args) {
+        String a = "Java ";
+        String b = "Programador ";
+        String c = a.concat(b + a.length());
+        System.out.println(c);
+    }
+}
