9c04b96c280772ddcf048ebebaf56e1484a695ca
Márcio Calisto <calistomarcio6@gmail.com>
2024-12-06 22:22:35 -0300

    Update codigo5.java

--- src/edu/marcio/atividades/codigo05.java
+++ src/edu/marcio/atividades/codigo05.java
@@ -2,13 +2,22 @@ package edu.marcio.atividades;
 
 public class codigo05 {
     public static void main(String[] args) {
-        int[][] matriz = { { 1, 2, 3 },
-                { 4, 5, 6 },
-                { 7, 8, 9 } };
-        int vResultado = 0;
+        // Declaração e inicialização de uma matriz 3x3
+        int[][] matriz = {
+                { 1, 2, 3 }, // Primeira linha
+                { 4, 5, 6 }, // Segunda linha
+                { 7, 8, 9 }  // Terceira linha
+        };
+
+        int vResultado = 0; // Variável para armazenar a soma dos elementos da diagonal principal
+
+        // Percorre as linhas da matriz
         for (int i = 0; i < matriz.length; i++) {
+            // Soma os elementos da diagonal principal (posição [i][i])
             vResultado += matriz[i][i];
         }
+
+        // Exibe o resultado da soma
         System.out.println("O resultado é: " + vResultado);
     }
 }
